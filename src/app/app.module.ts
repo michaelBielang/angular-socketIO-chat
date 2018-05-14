5bd67ffa49eb2283c88bcf26d7341397ac7ab610
Michael Bielang <bielang@codemerger.com>
2018-05-14 16:31:58 +0200

    add bootstrap 4.0 support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MessagesComponent} from './component/messages/messages.component';
 import {InputFieldComponent} from './component/input-field/input-field.component';
-import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import {FormsModule} from '@angular/forms';
+import { ChatRoomsComponent } from './component/chat-rooms/chat-rooms.component';
+import { ControlCenterComponent } from './component/control-center/control-center.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     MessagesComponent,
-    InputFieldComponent
+    InputFieldComponent,
+    ChatRoomsComponent,
+    ControlCenterComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
