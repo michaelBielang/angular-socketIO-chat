167eff497d3d76e45b3d1145637e57f22f67451c
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-10 13:21:04 +0200

    now grouping messages

---
+++ src/app/model/MessageSet.ts
@@ -0,0 +1,33 @@
+import {Message} from './Message';
+export class MessageSet {
+
+  _author;
+
+  constructor(private _messages: Message[]) {
+    this._author = _messages[0].author;
+    for (const msg of _messages) {
+      if (msg.author !== this.author) {
+        throw new Error ('not all messages given to MessageSet constructor are from the same author!');
+      }
+    }
+  }
+
+  add(msg: Message) {
+    if (msg.author !== this.author) {
+      throw new Error ('Message author doesn\'t match MessageSet author!');
+    }
+    this._messages.push(msg);
+  }
+
+  // function
+  get messages(): Message[] {
+    return this._messages;
+  }
+
+  get author(): string {
+    return this._author;
+  }
+
+}
+
+export default MessageSet;
