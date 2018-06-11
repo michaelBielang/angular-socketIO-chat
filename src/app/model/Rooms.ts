42b0ff23c7d8ae6d9fc872da97c674e4c17b2d6c
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-11 15:32:39 +0200

    added addMessage() function, still needs to be removed from messagedisplay

--- src/app/model/Rooms.ts
+++ src/app/model/Rooms.ts
@@ -2,10 +2,31 @@ import {User} from './User';
 
 export class Rooms {
 
+  constructor() {
+    numberUnreadMessages = 0;
+  }
+
   private _userList: User[];
-  private _roomMessages: String[];
+  private _messageSets: MessageSet[];
   private _OPList: Map<String, User>;
   private _VoiceList: Map<String, User>;
+  private lastAuthor: string;
+  private numberUnreadMessages;
+
+  addMessage(msg: Message, userCanSeeMessage: boolean): void {
+    if (! userCanSeeMessage) {
+      this.numberUnreadMessages += 1;
+    }
+    // if author matches, just add the msg to the last MessageSet
+    if (msg.author === this.lastAuthor && this.messageSets.length > 0) {
+      this.messageSets[this.messageSets.length - 1].add(msg);
+      // else, create a new temp MessageSet using the msg
+    } else {
+      // push the temp MessageSet to the MessagesDisplayComponent's messageSets
+      this.lastAuthor = msg.author;
+      this.messageSets.push(new MessageSet([msg]));
+    }
+  }
 
   hasOP(roomName: String): boolean {
     return true;
