e1f15913e0bcd17dd7775a5492ca299fcd60704f
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-11 15:55:04 +0200

    removed code that was moved to userService

--- src/app/component/messagesdisplay/messagesdisplay.component.ts
+++ src/app/component/messagesdisplay/messagesdisplay.component.ts
@@ -11,32 +11,7 @@ import {BackendResponse} from '../../model/BackendResponse';
 })
 export class MessagesDisplayComponent implements OnInit {
 
-  messageSets: MessageSet[];
-  lastauthor: string;
-
-  constructor(private socketService: SocketService,
-  ) {
-
-    // add messages one by one, creating a new MessageSet for each new author
-    this.lastauthor = 'nobody';
-    this.messageSets = [];
-    this.socketService.receiveEvents('MessageSendToRoom').subscribe((message: MessageEvent) => {
-      const obj: BackendResponse = JSON.parse(message.data);
-      this.add(new Message(obj.value.message, obj.value.email, new Date()));
-    });
-  }
-  add(msg: Message) {
-    // if author matches, just add the msg to the last MessageSet
-    if (msg.author === this.lastauthor && this.messageSets.length > 0) {
-      this.messageSets[this.messageSets.length - 1].add(msg);
-      // else, create a new temp MessageSet using the msg
-    } else {
-      // push the temp MessageSet to the MessagesDisplayComponent's messageSets
-      this.lastauthor = msg.author;
-      this.messageSets.push(new MessageSet([msg]));
-    }
-  }
-
+  constructor() {}
 
   ngOnInit() {
     console.log('messagedisplay oninit');
