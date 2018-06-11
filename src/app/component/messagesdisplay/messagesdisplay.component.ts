25d08dab7714890e382be57bc34980d08fbaf974
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-11 14:12:04 +0200

    added missing BackendendResponse import

--- src/app/component/messagesdisplay/messagesdisplay.component.ts
+++ src/app/component/messagesdisplay/messagesdisplay.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {SocketService} from '../../services/socketService/socket-service.service';
 import {Message} from '../../model/Message';
 import {MessageSet} from "../../model/MessageSet";
+import {BackendResponse} from "../../model/BackendResponse";
 
 @Component({
   selector: 'messages-display',
