d7d497e7626a878b82d9ab5e8fff48636e777ffc
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-11 15:36:57 +0200

    userService not subscribes to incoming messages and should save them

--- src/app/services/userService/user-service.service.ts
+++ src/app/services/userService/user-service.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Rooms} from '../../model/Rooms';
+import {BackendResponse} from "../../model/BackendResponse";
+import {Message} from "../../model/Message";
+import {SocketService} from "../socketService/socket-service.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +12,20 @@ export class UserServiceService {
   private _roomMap: Map<String, Rooms> = new Map<String, Rooms>();
   private activeRoom: string;
 
-  constructor() {
+  constructor(public socketService: SocketService) {
     // subscribe to messages
-
+    this.socketService.receiveEvents('MessageSendToRoom').subscribe((message: MessageEvent) => {
+      const obj: BackendResponse = JSON.parse(message.data);
+      this._roomMap.get(obj.value.roomName).addMessage(
+        // the message
+        new Message(obj.value.message, obj.value.email, new Date()),
+        // whether or not the message is actively being read
+        obj.value.roomName === this.activeRoom);
+    });
   }
   joinRoom(roomName: string): void {
     this._roomMap.set(roomName, new Rooms());
-    showRoom(roomName);
+    this.showRoom(roomName);
   }
   leaveRoom(roomName: string): void {
     this._roomMap.delete(roomName);
