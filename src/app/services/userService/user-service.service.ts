08536854b3900559c6583c85d3128622262eeea7
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-11 15:04:44 +0200

    skeleton join, leave, hide, show functions

--- src/app/services/userService/user-service.service.ts
+++ src/app/services/userService/user-service.service.ts
@@ -7,16 +7,26 @@ import {Rooms} from '../../model/Rooms';
 export class UserServiceService {
 
   private _roomMap: Map<String, Rooms> = new Map<String, Rooms>();
+  private activeRoom: string;
 
   constructor() {
+    // subscribe to messages
+
   }
   joinRoom(roomName: string): void {
     this._roomMap.set(roomName, new Rooms());
+    showRoom(roomName);
   }
   leaveRoom(roomName: string): void {
     this._roomMap.delete(roomName);
+    // don't hide the room; keep it visible
   }
-
+  showRoom(roomName: string): void {
+    this.activeRoom = roomName;
+  }
+  hideRoom(roomName: string): void {
+    // do... something?
+  }
 
   get roomMap(): Map<String, Rooms> {
     return this._roomMap;
