97b91e051e91907369e9c49b678ce83e6012974b
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-24 14:02:54 +0200

    added username to User Model

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,9 +1,11 @@
 export class User {
 
   private _email: String;
+  private _username: String;
 
-  constructor(email: String) {
+  constructor(email: String, username: String) {
     this._email = email;
+    this._username = username;
   }
 
   get email(): String {
@@ -12,5 +14,13 @@ export class User {
 
   set email(value: String) {
     this._email = value;
+  }
+
+  get username(): String {
+    return this._username;
+  }
+
+  set username(value: String) {
+    this._username = value;
   }
 }
