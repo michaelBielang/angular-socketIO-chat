3d8a4b7d7a311affda01ca350ebd60f42b082e8c
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2018-06-24 02:00:01 +0200

    update messagesdisp comp with corresponding messagesets when the active room changes

--- src/app/component/messagesdisplay/messagesdisplay.component.ts
+++ src/app/component/messagesdisplay/messagesdisplay.component.ts
@@ -4,6 +4,7 @@ import {SocketService} from '../../services/socketService/socket-service.service
 import {Message} from '../../model/Message';
 import {MessageSet} from '../../model/MessageSet';
 import {BackendResponse} from '../../model/BackendResponse';
+import {Subscription} from "rxjs/index";
 
 @Component({
   selector: 'app-messages-display',
@@ -13,6 +14,7 @@ import {BackendResponse} from '../../model/BackendResponse';
 export class MessagesDisplayComponent implements OnInit {
 
   private messageSets: MessageSet[];
+  private messageSubscription: Subscription;
 
   constructor(private userService: UserServiceService) {
 
@@ -23,12 +25,26 @@ export class MessagesDisplayComponent implements OnInit {
     // this.getMessages();
     console.log('activeRoom:', this.userService.activeRoom);
     console.log('roomMap:', this.userService.roomMap);
-    this.userService.roomMap.get(this.userService.activeRoom).messageSetsChanges.subscribe(
+    this.messageSubscription = this.userService.roomMap.get(this.userService.activeRoom).messageSetsChanges.subscribe(
       (next) => {
-        console.log('messageSets changed', next);
+        console.log('messageSets changed due to new messages (probably)', next);
         this.messageSets = next;
       }
     );
+    this.userService.activeRoomChanges.subscribe(
+      (next) => {
+        console.log('active room changed for message display', next);
+        this.messageSubscription.unsubscribe();
+        // get the new messageSet when the room changes
+        this.messageSubscription = this.userService.roomMap.get(this.userService.activeRoom).messageSetsChanges.subscribe(
+          (next) => {
+            console.log('messageSets changed due to different active Room', next);
+            this.messageSets = next;
+          }
+        );
+      }
+    );
   }
+  // todo subscribe to activeRoom; always update the messages-display to match
 
 }
